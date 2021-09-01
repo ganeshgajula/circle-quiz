6aa74659622a2837e10a6700e41392b1f72c9ff3
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-01 22:41:28 +0530

    feat: user logout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import logo from "../../assets/transparent-logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
 
 const Navbar = () => {
+  const {
+    authData: { isUserLoggedIn },
+    authDispatch,
+  } = useAuth();
+
+  const navigate = useNavigate();
+
   return (
     <nav className="flex justify-between items-center max-w-full px-10 py-2 shadow-md">
       <div className="flex items-center">
@@ -16,8 +24,15 @@ const Navbar = () => {
           Played Quizzes
         </Link>
       </div>
-      <button className="font-semibold px-5 py-2 bg-blue-500 text-white text-xl hover:bg-blue-700">
-        Login
+      <button
+        className="font-semibold px-5 py-2 bg-blue-500 text-white text-xl hover:bg-blue-700"
+        onClick={() => {
+          localStorage?.removeItem("userInfo");
+          authDispatch({ type: "LOGOUT_USER" });
+          navigate("/");
+        }}
+      >
+        {!isUserLoggedIn ? "Login" : "Logout"}
       </button>
     </nav>
   );
--- src/reducer/authReducer.ts
+++ src/reducer/authReducer.ts
@@ -11,7 +11,8 @@ export type AuthActionType =
       type: "SET_USER_CREDENTIALS";
       payload: UserData;
     }
-  | { type: "SET_USER_CREDENTIALS_FROM_LOCAL_STORAGE"; payload: UserData };
+  | { type: "SET_USER_CREDENTIALS_FROM_LOCAL_STORAGE"; payload: UserData }
+  | { type: "LOGOUT_USER" };
 
 export const authReducer = (state: AuthState, action: AuthActionType) => {
   switch (action.type) {
@@ -29,6 +30,14 @@ export const authReducer = (state: AuthState, action: AuthActionType) => {
         isUserLoggedIn: action.payload.loginStatus,
         userId: action.payload.userId,
         username: action.payload.username,
+      };
+
+    case "LOGOUT_USER":
+      return {
+        ...state,
+        isUserLoggedIn: false,
+        userId: "",
+        username: "",
       };
 
     default:
