22f1124078a9e21bd1bc1f56c785d19edded6911
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-03 08:01:32 +0530

    feat: show username after login

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { useAuth } from "../../context/AuthProvider";
 
 const Navbar = () => {
   const {
-    authData: { isUserLoggedIn },
+    authData: { isUserLoggedIn, userName },
     authDispatch,
   } = useAuth();
 
@@ -18,22 +18,27 @@ const Navbar = () => {
           <img src={logo} alt="brand-logo" className="h-14 w-44" />
         </Link>
         <Link to="/" className="ml-8 font-semibold">
-          All Quizzes
+          Home
         </Link>
         <Link to="/playedquizzes" className="ml-4 font-semibold">
           Played Quizzes
         </Link>
       </div>
-      <button
-        className="font-semibold px-5 py-2 bg-blue-500 text-white text-xl hover:bg-blue-700"
-        onClick={() => {
-          localStorage?.removeItem("userInfo");
-          authDispatch({ type: "LOGOUT_USER" });
-          navigate("/");
-        }}
-      >
-        {!isUserLoggedIn ? "Login" : "Logout"}
-      </button>
+      <div className="flex items-center">
+        <p className="mr-4 text-lg">Hi {userName}</p>
+        {isUserLoggedIn && (
+          <button
+            className="font-semibold px-4 py-2 bg-blue-500 text-white rounded-sm text-xl hover:bg-blue-700"
+            onClick={() => {
+              localStorage?.removeItem("userInfo");
+              authDispatch({ type: "LOGOUT_USER" });
+              navigate("/");
+            }}
+          >
+            Logout
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
