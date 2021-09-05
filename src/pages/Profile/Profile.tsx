9e4b5da05d2041bb875427efab82d361c909be05
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-05 22:37:18 +0530

    style: profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -50,8 +50,8 @@ export const Profile = () => {
     <>
       <Navbar />
       <div className="max-w-md mx-auto my-6">
-        <span className="flex items-center justify-between">
-          <h2>Edit Profile</h2>
+        <span className="flex items-center justify-between py-2 mb-10">
+          <h1 className="text-3xl">Account Details</h1>
           <button
             className="bg-red-500 text-white font-medium px-2 py-1 rounded-md"
             onClick={() => {
@@ -63,24 +63,60 @@ export const Profile = () => {
             Logout
           </button>
         </span>
-        <form onSubmit={updateProfileHandler} className="flex flex-col">
-          <div>
-            <input
-              type="text"
-              placeholder="firstname"
-              value={firstname}
-              onChange={(e) => setFirstname(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="lastname"
-              value={lastname}
-              onChange={(e) => setLastname(e.target.value)}
-            />
-          </div>
-          <input type="email" placeholder={user?.email} />
-          <button type="submit">Save</button>
-        </form>
+        <div className="border border-gray-200 rounded-lg shadow-md p-4">
+          <h2 className="font-medium text-xl py-3">Edit Profile</h2>
+          <form onSubmit={updateProfileHandler}>
+            <div className="flex justify-between items-center my-4">
+              <div className="flex flex-col">
+                <label
+                  htmlFor="firstname"
+                  className="text-left pb-1 font-medium text-gray-700"
+                >
+                  firstname
+                </label>
+                <input
+                  type="text"
+                  className="border border-gray-300 p-1 rounded-md"
+                  placeholder="firstname"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="lastname"
+                  className="text-left pb-1 font-medium text-gray-700"
+                >
+                  lastname
+                </label>
+                <input
+                  type="text"
+                  className="border border-gray-300 p-1 rounded-md"
+                  placeholder="lastname"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-left pb-1 font-medium text-gray-700"
+              >
+                email
+              </label>
+              <div className="p-1 border border-gray-300 rounded-md text-left cursor-not-allowed">
+                {user?.email}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="my-4 py-1 px-3 rounded-md bg-blue-500 text-white font-semibold text-lg"
+            >
+              Save
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
