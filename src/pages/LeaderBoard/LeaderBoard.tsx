079a354a01ef9bcd751d3e1ab05e8019fc614a4f
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-05 21:03:50 +0530

    style: leaderboard

--- src/pages/LeaderBoard/LeaderBoard.tsx
+++ src/pages/LeaderBoard/LeaderBoard.tsx
@@ -33,17 +33,27 @@ export const LeaderBoard = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <h2>Inside leaderboard</h2>
-        <ul>
+      <div className="my-8 max-w-3xl mx-auto">
+        <h1 className="text-3xl font-semibold">LeaderBoard</h1>
+        <div className="flex items-center justify-between mt-8 mb-1 px-2 font-semibold text-lg">
+          <span>Sr.No</span>
+          <span>Name</span>
+          <span>Quiz Name</span>
+          <span>Score</span>
+        </div>
+        <ul className="mb-4">
           {status === "loading" && <p>Loading...</p>}
           {status === "success" &&
-            leaderBoardToppers.map((leaderBoard) => (
-              <li key={leaderBoard._id}>
-                <span>{leaderBoard.quizId.quizName}</span>
+            leaderBoardToppers.map((leaderBoard, index) => (
+              <li
+                key={leaderBoard._id}
+                className="border border-gray-200 flex items-center justify-between p-3"
+              >
+                <span>{index + 1}</span>
                 <span>
                   {leaderBoard.userId.firstname} {leaderBoard.userId.lastname}
                 </span>
+                <span>{leaderBoard.quizId.quizName}</span>
                 <span>{leaderBoard.score}</span>
               </li>
             ))}
